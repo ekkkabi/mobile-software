fe794ee6b22d7f23c694b01404cc46150cbb1884
ekkk1126 <cabi0203@naver.com>
2023-12-06 19:53:31 +0900

    Fragment; 동적 프래그먼트

--- FragmentSample/app/src/main/java/com/example/fragmentsample/MainActivity.kt
+++ FragmentSample/app/src/main/java/com/example/fragmentsample/MainActivity.kt
@@ -1,7 +1,7 @@
 package com.example.fragmentsample
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.example.fragmentsample.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -10,7 +10,11 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
+        
+        supportFragmentManager.beginTransaction().apply {
+            add(R.id.fragmentA, FragmentA())
+            commit()
+        }
 
     }
 }
